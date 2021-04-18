7acca6869ace3cb7c2d7de021ba8c4d33bad86bc
absalon221 <60877947+absalon221@users.noreply.github.com>
2021-04-18 14:01:51 +0200

    zad. 1.2

--- main.py
+++ main.py
@@ -4,18 +4,23 @@ from pydantic import BaseModel
 app = FastAPI()
 app.counter = 0
 
-class HelloResp(BaseModel):
-    msg: str
+#class HelloResp(BaseModel):
+#    msg: str
 
 @app.get("/")
 def root():
     return {"message": "Hello world!"}
 
-@app.get("/hello/{name}", response_model=HelloResp)
+@app.get("/hello/{name}")   # , response_model=HelloResp)
 def hello_name_view(name: str):
-    return HelloResp(msg=f"Hello {name}")
+    return f"Hello {name}"
+    # return HelloResp(msg=f"Hello {name}")
 
 @app.get('/counter')
 def counter():
     app.counter += 1
     return str(app.counter)
+
+@app.post('/method', status_code = 201)
+def method():
+    return {"method": "POST"}
--- test_1.py
+++ test_1.py
@@ -8,7 +8,7 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    assert response.json() == {"message": "Hello world!"}
 
 @pytest.mark.parametrize("name", ["Zenek", "Marek", "Alojzy Niezdąży"])
 def test_hello_name(name):
@@ -19,8 +19,13 @@ def test_hello_name(name):
 def test_counter():
     response = client.get(f"/counter")
     assert response.status_code == 200
-    assert response.text == "1"
+    assert response.text == '"1"'
     # 2nd Try
     response = client.get(f"/counter")
     assert response.status_code == 200
-    assert response.text == "2"
+    assert response.text == '"2"'
+    
+def test_method():
+    response = client.post(f"/method")
+    assert response.status_code == 201  
+    assert response.json() == {"method": "POST"}
