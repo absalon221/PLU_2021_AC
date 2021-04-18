089884b1ec2f82d8955407a2b9eaa146bafef48b
absalon221 <60877947+absalon221@users.noreply.github.com>
2021-04-18 18:32:02 +0200

    Zad. 1. 2. - więcej pustych haseł

--- test_current.py
+++ test_current.py
@@ -5,5 +5,5 @@ from main import app
 client = TestClient(app)
 
 def test_auth():
-    response = client.get(f"/auth?password=  &password_hash=16b7aa7f7e549ba129c776bb91ce1e692da103271242d44a9bc145cf338450c90132496ead2530f527b1bd7f50544f37e7d27a2d2bbb58099890aa320f40aca9")
+    response = client.get(f"/auth?password=&password_hash=16b7aa7f7e549ba129c776bb91ce1e692da103271242d44a9bc145cf338450c90132496ead2530f527b1bd7f50544f37e7d27a2d2bbb58099890aa320f40aca9")
     assert response.status_code == 401
