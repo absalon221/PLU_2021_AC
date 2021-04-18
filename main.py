8aa7dc954f100fb7566874c3738aa62092075bf6
absalon221 <60877947+absalon221@users.noreply.github.com>
2021-04-18 19:08:24 +0200

    Zad. 1.4

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, Response, status
-import hashlib
+from pydantic import BaseModel
+import hashlib, datetime
 
 app = FastAPI()
+app.counter = 0
     
 @app.get('/auth', status_code = 401)
 def auth(password:str, password_hash:str, response:Response):
@@ -9,3 +11,11 @@ def auth(password:str, password_hash:str, response:Response):
         calculated_hash = str(hashlib.sha512(bytes(password, encoding='utf-8')).hexdigest())
         if password_hash == calculated_hash:
             response.status_code = 204
+            
+@app.post('/register', status_code = 201)
+def register(name:str, surname:str):
+    app.counter += 1
+    register_date = datetime.date.today()
+    vaccination_date = register_date + datetime.timedelta(days = (len(name) + len(surname)))
+   
+    return {"id": str(app.counter),"name": name,"surname": surname, "register_date": str(register_date),  "vaccination_date":str(vaccination_date)}
--- test_current.py
+++ test_current.py
@@ -7,3 +7,13 @@ client = TestClient(app)
 def test_auth():
     response = client.get(f"/auth?password=&password_hash=16b7aa7f7e549ba129c776bb91ce1e692da103271242d44a9bc145cf338450c90132496ead2530f527b1bd7f50544f37e7d27a2d2bbb58099890aa320f40aca9")
     assert response.status_code == 401
+    
+def test_register():
+    #1
+    response = client.post(f"/register?name=Jan&surname=Nowak")
+    assert response.status_code == 201
+    assert response.json() == {"id": "1","name": "Jan","surname": "Nowak", "register_date": "2021-04-18",  "vaccination_date": "2021-04-26"}
+    #2
+    response = client.post(f"/register?name=Blob&surname=Blaab")
+    assert response.status_code == 201
+    assert response.json() == {"id": "2","name": "Blob","surname": "Blaab", "register_date": "2021-04-18",  "vaccination_date": "2021-04-27"}    
