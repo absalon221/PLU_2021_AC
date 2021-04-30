5f8e1c70df4eadb21191a975bf1f88dc846e5e15
absalon221 <60877947+absalon221@users.noreply.github.com>
2021-04-30 21:43:57 +0200

    Zad. 3.3

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 security = HTTPBasic()
 
-app.stored_login_session = ""
-app.stored_login_token = ""
+app.stored_login_session = []
+app.stored_login_token = []
     
 @app.get("/hello")
 def print_date(request: Request, response: Response):
@@ -30,7 +30,10 @@ def login_session(response: Response, credentials: HTTPBasicCredentials = Depend
     
     session_token = sha256(f"{credentials.username}{credentials.password}".encode()).hexdigest()
     response.set_cookie(key="session_token", value=session_token)
-    app.stored_login_session = session_token
+    app.stored_login_session.append(session_token)
+    
+    if len(app.stored_login_session) > 5:
+        app.stored_login_session.pop(0)
         
 
 @app.post("/login_token", status_code = 201)
@@ -43,5 +46,39 @@ def login_token(response: Response, credentials: HTTPBasicCredentials = Depends(
     
     session_token = sha256(f"{credentials.username}{credentials.password}".encode()).hexdigest()
     response.set_cookie(key="value_token", value=session_token)
-    app.stored_login_token = session_token
+    app.stored_login_token.append(session_token)
+    
+    if len(app.stored_login_token) > 5:
+        app.stored_login_token.pop(0)
+    
     return {"token": session_token}
+
+@app.get("/welcome_session", status_code=200)
+def welcome_session(response: Response, session_token: str = Cookie(None), format = None):
+    if session_token not in app.stored_login_sessions:
+        raise HTTPException(status_code=401)
+    
+    if format == 'json':
+        response.headers["content-type"] = "application/json"
+        return {"message": "Welcome!"}
+    elif format == 'html':
+        response.headers["content-type"] = "text/html"
+        return "<h1>Welcome!</h1>"
+    else:
+        response.headers["content-type"] = "text/plain"
+        return "Welcome!"
+        
+@app.get("/welcome_token", status_code=200)
+def welcome_token(response: Response, token: str, format = None):
+    if token not in app.stored_login_token:
+        raise HTTPException(status_code=401)
+    
+    if format == 'json':
+        response.headers["content-type"] = "application/json"
+        return {"message": "Welcome!"}
+    elif format == 'html':
+        response.headers["content-type"] = "text/html"
+        return "<h1>Welcome!</h1>"
+    else:
+        response.headers["content-type"] = "text/plain"
+        return "Welcome!"
