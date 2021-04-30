b0d27442094f40de45938fd41ce8e5359eed948c
absalon221 <60877947+absalon221@users.noreply.github.com>
2021-04-30 21:19:05 +0200

    FYUJHGVGHDXCFGDCHJGBJMN

--- main.py
+++ main.py
@@ -3,17 +3,16 @@ from fastapi.templating import Jinja2Templates
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from datetime import date
 from pydantic import BaseModel
-import base64, secrets
 from hashlib import sha256
+import secrets
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 security = HTTPBasic()
-#app.secret_key = "dfghjk54t576weufhuj"
-#app.access_tokens = [0, 0]
 
-class TokenResp(BaseModel):
-    token: str
+app.stored_login_session = ""
+app.stored_login_token = ""
     
 @app.get("/hello")
 def print_date(request: Request, response: Response):
@@ -25,18 +24,24 @@ def print_date(request: Request, response: Response):
 def login_session(response: Response, credentials: HTTPBasicCredentials = Depends(security)):
     correct_username = secrets.compare_digest(credentials.username, "4dm1n")
     correct_password = secrets.compare_digest(credentials.password, "NotSoSecurePa$$")
+    
     if not (correct_username and correct_password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-    #app.access_tokens[0]=1
-    response.set_cookie(key="session_token", value=1)
+        raise HTTPException(status_code=401)
+    
+    session_token = sha256(f"{credentials.username}{credentials.password}".encode()).hexdigest()
+    response.set_cookie(key="session_token", value=session_token)
+    app.stored_login_session = session_token
         
 
-@app.post("/login_token", status_code = 201, response_model=TokenResp)
+@app.post("/login_token", status_code = 201)
 def login_token(response: Response, credentials: HTTPBasicCredentials = Depends(security)):
     correct_username = secrets.compare_digest(credentials.username, "4dm1n")
     correct_password = secrets.compare_digest(credentials.password, "NotSoSecurePa$$")
+    
     if not (correct_username and correct_password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-    #app.access_tokens[1]=2
-    response.set_cookie(key="value_token", value=2)
-    return TokenResp(token=2)
+        raise HTTPException(status_code=401)
+    
+    session_token = sha256(f"{credentials.username}{credentials.password}".encode()).hexdigest()
+    response.set_cookie(key="value_token", value=session_token)
+    app.stored_login_token = session_token
+    return {"token": session_token}
