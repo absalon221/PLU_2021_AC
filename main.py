d0932bac3b953a69f89903a06df21cdaec0de493
absalon221 <60877947+absalon221@users.noreply.github.com>
2021-04-30 21:20:14 +0200

    HTTPException

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, Response, Cookie, Depends
+from fastapi import FastAPI, Request, Response, Cookie, Depends, HTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from datetime import date
