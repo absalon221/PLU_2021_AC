bc79b46483f8f2b68068e2d2c5824def1cef31e6
absalon221 <60877947+absalon221@users.noreply.github.com>
2021-04-30 17:34:40 +0200

    Zadanie 3.1

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from datetime import date
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/hello")
+def print_date(request: Request):
+    return_date = date.today()
+    return templates.TemplateResponse("hello.html", {"date": return_date})
