e9fedba31a65930b7fd4504396e9d00c62ad3ce7
absalon221 <60877947+absalon221@users.noreply.github.com>
2021-04-30 20:09:43 +0200

    3.2 - better pass comparison

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from fastapi import FastAPI, Request, Response, Cookie, Depends
 from fastapi.templating import Jinja2Templates
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from datetime import date
-import base64
+import base64, secrets
 from hashlib import sha256
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 security = HTTPBasic()
 #app.secret_key = "dfghjk54t576weufhuj"
-app.access_tokens = [0, 0]
+#app.access_tokens = [0, 0]
 
 @app.get("/hello")
 def print_date(request: Request, response: Response):
@@ -19,18 +19,21 @@ def print_date(request: Request, response: Response):
 
 @app.post("/login_session")
 def login_session(credentials: HTTPBasicCredentials = Depends(security)):
-    if not (credentials.username == "4dm1n") or not (credentials.password == "NotSoSecurePa$$"):
-        raise HTTPException(status_code=401)
-    #session_token = sha256(f"{credentials.username}{credentials.password}{app.secret_key}".encode()).hexdigest()
-    #app.access_tokens.append(session_token)
-    app.access_tokens[0]=1
+    correct_username = secrets.compare_digest(credentials.username, "4dm1n")
+    correct_password = secrets.compare_digest(credentials.password, "NotSoSecurePa$$")
+    if not (correct_username and correct_password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    #app.access_tokens[0]=1
     response.set_cookie(key="session_token", value=1)
     return
         
 
 @app.post("/login_token")
 def login_token(credentials: HTTPBasicCredentials = Depends(security)):
-    if not (credentials.username == "4dm1n") or not (credentials.password == "NotSoSecurePa$$"):
-        raise HTTPException(status_code=401)
-    app.access_tokens[1]=2
+    correct_username = secrets.compare_digest(credentials.username, "4dm1n")
+    correct_password = secrets.compare_digest(credentials.password, "NotSoSecurePa$$")
+    if not (correct_username and correct_password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    #app.access_tokens[1]=2
+    response.set_cookie(key="value_token", value=2)
     return {"token": 2}
