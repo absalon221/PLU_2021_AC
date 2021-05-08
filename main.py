d7e6bd55b8dd4ed57291758863f06795c647c2c4
absalon221 <60877947+absalon221@users.noreply.github.com>
2021-05-08 18:22:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,20 +6,18 @@ from typing import List
 
 app = FastAPI()
 
+#######
 
 @app.on_event("startup")
 async def startup():
     app.db_connection = sqlite3.connect("northwind.db")
     app.db_connection.text_factory = lambda b: b.decode(errors="ignore")  # northwind specific
 
-
 @app.on_event("shutdown")
 async def shutdown():
     app.db_connection.close()
     
-@app.get("/")
-def hello(status_code=200):
-    return
+#######
     
 @app.get("/categories", status_code=200)
 async def categories():
